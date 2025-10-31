1685b1977a051196a212f79f7f4e8e7d22f0232a
seemakanji <seema@alykanji.com>
2025-10-31 15:56:01 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,4 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = { experimental: { appDir: true } };
+module.exports = nextConfig;
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
