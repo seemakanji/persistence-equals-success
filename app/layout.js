f3b2ff2b15dfe6772fdb3d6b88e6fb1d20092717
seemakanji <seema@alykanji.com>
2025-10-31 17:01:35 -0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export default function Page() {
+  return (
+    <main style={{ padding: "4rem", fontFamily: "sans-serif" }}>
+      <h1>Persistence Equals Success</h1>
+      <h2>My Journey to Motherhood</h2>
+      <p>
+        Welcome to the official website for Seema Kanji’s memoir,
+        <em> Persistence Equals Success (My Journey to Motherhood)</em>.
+      </p>
+    </main>
+  );
+}
